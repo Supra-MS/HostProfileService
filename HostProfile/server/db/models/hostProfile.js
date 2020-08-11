c5ee94f9639eab23ee225545380689fa09fca613
Supra-MS <suprabharathi.m.s@gmail.com>
2020-08-11 11:23:58 -0700

    Add seeding db

---
+++ HostProfile/server/db/models/hostProfile.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const db = require('../index');
+mongoose.Promise = global.Promise;
+
+const hostProfileSchema = new mongoose.Schema({
+  _id: {
+    type: Number,
+    unique: true,
+    index: true
+  },
+  host_url: String,
+  host_name: String,
+  host_about: String,
+  host_messages: String,
+  host_identity_verified: Boolean,
+  host_is_superHost: Boolean,
+  host_has_profile_pic: Boolean,
+  host_has_coHost: Boolean,
+  host_response_time: Number,
+  host_listings_count: Number,
+  host_verifications: Array,
+  host_languages: Array
+},
+{ timestamps: true }
+);
+
+hostProfileSchema.method("toJSON", function() {
+  const { __v, _id, ...object } = this.toObject();
+  object.id = _id;
+  return object;
+});
+
+const HostProfile = mongoose.model('HostProfile', hostProfileSchema);
+
+module.exports = HostProfile;
---
+++ HostProfile/server/db/seed.js
@@ -0,0 +1,16 @@
+const db  = require('./index');
+const HostProfile = require('./models/hostProfile');
+const generateHostProfiles = require('../helpers/generateHostProfiles')
+
+const sampleHostProfiles = generateHostProfiles();
+
+const insertSampleHostProfiles = function() {
+
+  HostProfile.create(sampleHostProfiles)
+    .then(() => {
+      console.log('Successfully inserted 100 sample host profiles');
+      // db.disconnect();
+    });
+};
+
+insertSampleHostProfiles();
