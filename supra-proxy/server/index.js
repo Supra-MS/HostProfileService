0ba3b4f04e5b7b2aec042e5ac71ec0ffaa2197f3
Supra-MS <suprabharathi.m.s@gmail.com>
2020-08-28 22:56:11 -0700

    Able to render all 4 services for same home detail page

--- supra-proxy/server/index.js
+++ supra-proxy/server/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
 const proxy = require('express-http-proxy');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(__dirname + '../client'));
+app.use(cors());
+app.use(express.static(__dirname + '/../client'));
+
+app.get('/:id', (req, res) => {
+  res.sendFile(path.join(__dirname + './../client/index.html'));
+});
 
 app.use('/proxy', proxy('www.google.com', {
   filter: function(req, res) {
@@ -11,24 +18,62 @@ app.use('/proxy', proxy('www.google.com', {
  }
 }));
 
-app.use('/hostInfo', proxy('http://localhost:3001', {
+app.use('/homeDesc', proxy('http://localhost:3002', {
   proxyReqPathResolver: function (req) {
-    let parts = req.url.split('/');
+    let parts = req.url.split('?');
+    let pathname = req.url.split('/')[1];
     let queryString = parts[1];
-    let updatedPath = parts[0];
-    console.log('🤡',parts, req.url, '🏵', queryString, updatedPath );
-    return (!queryString.length) ? '/' : `/hostInfo/${queryString}`;
+    console.log('🏓 home', parts, queryString, '🪀', pathname)
+    return !pathname ? '/' : `/homeDesc${pathname}`;
   }
 }));
 
-app.use('/homeDesc', proxy('http://localhost:3002', {
+app.use('/hostInfo', proxy('http://localhost:3006', {
   proxyReqPathResolver: function (req) {
-    let parts = req.url.split('/');
+    let parts = req.url.split('?');
     let queryString = parts[1];
-    let updatedPath = parts[0];
-    return (!queryString.length) ? '/' : `/homeDesc/${queryString}`;
+    let pathname = req.url.split('/')[1];
+    console.log('🏓 host', parts, queryString, '🪀', pathname)
+    return !pathname ? '/' : `/hostInfo${pathname}`;
   }
 }));
+
+app.use('/images', proxy('http://localhost:3001', {
+  proxyReqPathResolver: function (req) {
+    let parts = req.url.split('?');
+    let queryString = parts[1];
+    let pathname = req.url.split('/')[1];
+    console.log('🏓 images', parts, queryString, '🪀', pathname)
+    return !pathname ? '/' : `/${pathname}`;
+  }
+}));
+
+app.use('/pricing', proxy('http://localhost:3003', {
+  proxyReqPathResolver: function (req) {
+    let parts = req.url.split('?');
+    let queryString = parts[1];
+    let pathname = req.url.split('/')[1];
+    console.log('🏓 pricing', parts, queryString, '🪀', pathname)
+    return !pathname ? '/' : `/pricing${pathname}`;
+  }
+}));
+
+app.use('/reviews', proxy('http://localhost:3004', {
+  proxyReqPathResolver: function (req) {
+    let parts = req.url.split('?');
+    let queryString = parts[1];
+    return !queryString ? '/' : `/hostInfo/${queryString}`;
+  }
+}));
+
+app.use('/location', proxy('http://localhost:3005', {
+  proxyReqPathResolver: function (req) {
+    let parts = req.url.split('?');
+    let queryString = parts[1];
+    return !queryString ? '/' : `/hostInfo/${queryString}`;
+  }
+}));
+
 
 app.listen(PORT, () => {
   console.log(`Proxy Server listening on port http://localhost:${PORT}`);
